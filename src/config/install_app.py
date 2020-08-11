492d013b77a57a93d36cb997b11e8393a50aea6f
fhara <49302325+fhara@users.noreply.github.com>
2020-08-11 19:56:13 +0900

    Update install_app.py

--- src/config/install_app.py
+++ src/config/install_app.py
@@ -82,7 +82,7 @@ parms = "-appname thinkdemoappwar"
 parms += " -node " + node + " -server " + server
 parms += " -nouseMetaDataFromBinary"
 parms += " -MapWebModToVH [[.* .* default_host]]"
-parms += " -MapResRefToEJB [[ "Think Web Application" "" thinkdemoapp-0.0.1-snapshot.war,WEB-INF/web.xml db2con javax.sql.DataSource db2con "" "" ]]"
+parms += ' -MapResRefToEJB [[ "Think Web Application" "" thinkdemoapp-0.0.1-snapshot.war,WEB-INF/web.xml db2con javax.sql.DataSource db2con "" "" ]]'
 
 print "parms:"
 print parms
